7f043a806ac12daa5a9d878c5dd9050eec6296c8
TalebAlkhbsh <123958807+TalebAlkhbsh@users.noreply.github.com>
2023-03-01 19:18:08 +0300

    Add files via upload

---
+++ Home2.java
@@ -0,0 +1,35 @@
+package Homework1;
+
+public class Home2{
+    public static void main(String[] args)
+
+
+    {
+        int intArray[] = {12,15,17};
+
+
+        System.out.println("Contents of intArray[] before assignment:");
+        for (int i=0; i<intArray.length; i++)
+            System.out.print(intArray[i] + " ");
+
+
+        int copyArray[] = new int[intArray.length];
+
+
+        copyArray = intArray;
+
+
+        copyArray[1]++;
+
+
+        System.out.println("\nContents of intArray[]:");
+        for (int i=0; i<intArray.length; i++)
+            System.out.print(intArray[i] + " ");
+
+        System.out.println("\nContents of copyArray[]:");
+        for (int i=0; i<copyArray.length; i++)
+            System.out.print(copyArray[i] + " ");
+    }
+}
+
+
---
+++ Home3.java
@@ -0,0 +1,26 @@
+package Homework1;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+public class Home3 {
+    public class Main {
+        public static void main(String[] args) {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            removeElements(array);
+        }
+
+        static void removeElements(int @NotNull [] array) {
+            Random r = new Random();
+            while (array.length > 0) {
+                int index = r.nextInt(array.length);
+                System.out.println("INDEX = " + index + ", ELEMENT = " + array[index]);
+                int[] array1 = new int[array.length - 1];
+                for (int i = 0; i < index; i++)
+                    array1[i] = array[i];
+                for (int i = index; i < array.length - 1; i++)
+                    array1[i] = array[i + 1];
+                array = array1;
+            }
+        }
+    }
+}
